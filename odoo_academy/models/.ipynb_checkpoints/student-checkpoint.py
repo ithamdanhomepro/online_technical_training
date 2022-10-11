294b9cb7df0f0f36742ba961d35eede36d822d37
Issam Hamdan <i.hamdan@homeprotech.com>
2022-10-11 16:54:53 +0000

    (Add) commit

--- odoo_academy/models/.ipynb_checkpoints/student-checkpoint.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api
-
-class Student(models.Model):
-    
-    _name: 'academy.student'
-    _description = 'Student Info'
-    _order = "id desc"
--- odoo_academy/models/__init__.py
+++ odoo_academy/models/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 
 from . import course
-from . import student
+#from . import student
 
