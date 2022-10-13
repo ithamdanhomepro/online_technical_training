a1349758a92627e78a4932e23db5c1371fdea1f6
Issam Hamdan <ithamdan@hotmeprotech.com>
2022-10-13 19:54:17 +0000

    (Added) course fields base_price additional_fee and total_price with validaton

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -25,11 +25,10 @@ class Course(models.Model):
     
     total_price = fields.Float(string='Total Price', readonly=True)
     
-    #add methods
-    @api.onchange('base_price', 'addtional_fee')
+    @api.onchange('base_price', 'additional_fee')
     def _onchange_total_price(self):
         
-        if self.base_price < 0.00:
+        if self.base_price < 0.0:
             raise UserError('Base Price can not be set as Negative.')
             
         self.total_price = self.base_price + self.additional_fee
@@ -38,5 +37,5 @@ class Course(models.Model):
     def _check_additional_fee(self):
         for record in self:
             if record.additional_fee < 10.00:
-                raise VaalidationError('Addtional Fees cannot be less than 10.00: %s' % record.additional_fee)
+                raise ValidationError('Addtional Fees cannot be less than 10.00: %s' % record.additional_fee)
     
