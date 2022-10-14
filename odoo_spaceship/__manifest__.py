2fd5fc93b17c352ea19df872c85a24a196a07f4c
Issam Hamdan <ithamdan@hotmeprotech.com>
2022-10-14 17:54:40 +0000

    (Add Spaceship model and update Academy

--- odoo_spaceship/__manifest__.py
+++ odoo_spaceship/__manifest__.py
@@ -19,7 +19,8 @@
     
     'data': [
         'security/spaceship_security.xml',
-        'security/ir.model.access.csv'
+        'security/ir.model.access.csv',
+        'views/spaceship_menuitems.xml',
     
     ],
     
