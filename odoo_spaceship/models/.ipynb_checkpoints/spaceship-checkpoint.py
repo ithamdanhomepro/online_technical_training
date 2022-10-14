fdbdb3baa5e2f55b402c5cc545798dd5cad2335e
Issam Hamdan <ithamdan@hotmeprotech.com>
2022-10-14 16:02:40 +0000

    (Add Spaceship model and update Academy

---
+++ odoo_spaceship/models/.ipynb_checkpoints/spaceship-checkpoint.py
@@ -0,0 +1,26 @@
+from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
+
+class Spaceship(models.Model):
+    
+    _name = 'academy.spaceship'
+    _description = 'Spaceship Info'
+    
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text(string='Description')
+    
+    ship_length = fields.Float(string='Ship Length')
+    ship_width = fields.Float(string='Ship Width')
+    ship_height = fields.Float(string='Ship Height')
+    
+    fuel_type = fields.Text(string='Fuel Type', default='Nitrogen', readonly=True)
+    
+    ship_type = fields.selection(string='Ship Type',
+                                selection=[('rocket','Rocket'),
+                                           ('capsule', 'Capsule')],
+                                copy=False)
+    
+    no_passengers = fields.Integer(string='Number of Passengers', default=1)
+    
+    active = fields.Boolean(string='Active', default=True)
+    
--- odoo_spaceship/models/spaceship.py
+++ odoo_spaceship/models/spaceship.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class Spaceship(models.Model):
     
@@ -6,5 +7,20 @@ class Spaceship(models.Model):
     _description = 'Spaceship Info'
     
     name = fields.Char(string='Title', required=True)
-    deswription = fields.Text(string='Description')
+    description = fields.Text(string='Description')
+    
+    ship_length = fields.Float(string='Ship Length')
+    ship_width = fields.Float(string='Ship Width')
+    ship_height = fields.Float(string='Ship Height')
+    
+    fuel_type = fields.Text(string='Fuel Type', default='Nitrogen', readonly=True)
+    
+    ship_type = fields.selection(string='Ship Type',
+                                selection=[('rocket','Rocket'),
+                                           ('capsule', 'Capsule')],
+                                copy=False)
+    
+    no_passengers = fields.Integer(string='Number of Passengers', default=1)
+    
+    active = fields.Boolean(string='Active', default=True)
     
