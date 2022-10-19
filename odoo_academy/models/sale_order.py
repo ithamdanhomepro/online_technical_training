0750fa7fed7b0de2015f2a8040a67265bc337db0
Issam Hamdan <ithamdan@hotmeprotech.com>
2022-10-19 00:28:17 +0000

    (Add) Sales Order inherit from Sale

--- odoo_academy/models/sale_order.py
+++ odoo_academy/models/sale_order.py
@@ -2,8 +2,7 @@
 from odoo import models, fields, api
 
 class SaleOrder(models.Model):
-    _inheret = 'sale.order' 
-    _description = 'Sale Order Inherited'
+    _inherit = 'sale.order'
     
     session_id = fields.Many2one(comodel_name='academy.session',
                                  string='Related Session',
