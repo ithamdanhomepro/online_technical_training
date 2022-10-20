298b724b479f19e617d4b007bfad4c78ff6a7c89
Issam Hamdan <ithamdan@hotmeprotech.com>
2022-10-20 17:00:11 +0000

    (Add) session report templates

--- odoo_academy/controllers/.ipynb_checkpoints/academy_conrollers-checkpoint.py
+++ odoo_academy/controllers/.ipynb_checkpoints/academy_conrollers-checkpoint.py
@@ -2,19 +2,19 @@ from odoo import http
 
 class Academy(http.Controller):
     
-    @http.route('/academy/', auth='public', website=True)
+    @http.route('/academy/', auth='public', website=True, sitemap=True)
     def index(self, **kw):
-        return "Hello, World"
+        return "Hello, World!"
     
     
-    @http.route('/academy/courses/', auth='public', website=True)
+    @http.route('/academy/courses/', auth='public', website=True, sitemap=False)
     def courses(self, **kw):
-        courses = http.rerquest.env['academy.course'].search([])
+        courses = http.request.env['academy.course'].search([])
         return http.request.render('odoo_academy.course website', {
             'courses': courses,
         })
     
-    @http.route('"/academy/<model("academy.session"):session>/', auth='public', website="true")
+    @http.route('"/academy/<model("academy.session"):session>/', auth='public', website="True", sitemap=False)
     def session(self, session):
         return http.request.render('odoo_academy.session_website', {
             'session': session,
