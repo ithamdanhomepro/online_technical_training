5b870d52c959f673ef44e0ef9fc7a9f7a377ae8f
Issam <115102268+ithamdanhomepro@users.noreply.github.com>
2022-10-20 13:07:11 -0500

    Update mission.py

--- odoo_spaceship/models/mission.py
+++ odoo_spaceship/models/mission.py
@@ -1,27 +1,56 @@
+from datetime import timedelta
 
-from odoo import models, fields, api
-from datetime import date
+from odoo import api, fields, models
 
 class Mission(models.Model):
+    _name = "space_mission.mission"
+    _description = "Space Missions"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     
-    _name = 'academy.mission'
-    _description = 'Missions Info'
+    active = fields.Boolean(default=True)
+    name = fields.Char(name='Mission Name')
+    priority = fields.Selection(selection=[("1","1"),
+                                          ("2","2"),
+                                          ("3","3"),
+                                          ("4","4"),
+                                          ("5","5"),])
+    type = fields.Selection(selection=[('supply_delivery','Supply Delivery'),
+                                   ('exploration', 'Exploration'),
+                                   ('war_efforts', 'War Efforts'),
+                                   ('transport', 'Transportation')],
+                        string='Mission Type',)
+    captain_id = fields.Many2one(comodel_name='res.users',
+                                string='Captain')
     
-    spaceship_id = fields.Many2one(comodel_name='academy.spaceship',
-                                   string='Spaceship',
-                                   ondelete='cascade',
-                                   required=True)
+    launch_date = fields.Datetime(string="Launch Date",
+                                  default=fields.Date.today)
+    duration = fields.Float(string='Duration',
+                           default="1")
+    return_date = fields.Datetime(string="Return Date",
+                          compute='_compute_return_date',
+                          inverse='_inverse_return_date',
+                          store=True)
     
-    #now create a related field to spaceship using the id to get the spaceship name (use related attribute)
-    name = fields.Char(string='Title', related='spaceship_id.name')
+    fuel_needed = fields.Float(string="Needed Fuel")
+    fuel_current = fields.Float(string="Current Fuel")
     
-    crewmember_ids = fields.Many2many(comodel_name='res.partner', string='Crea Members')
+    spaceship_id = fields.Many2one(comodel_name='space_mission.spaceship')
+    project_ids = fields.One2many(comodel_name='project.project',
+                                 inverse_name='mission_id',
+                                 string='Projects')
     
-    launch_date = fields.Date(string='Launch Date')
-    return_date = fields.Date(string='Return Date')
+    @api.depends('launch_date', 'duration')
+    def _compute_return_date(self):
+        for record in self:
+            if not (record.launch_date and record.duration):
+                record.return_date = record.launch_date
+            else:
+                duration = timedelta(days=record.duration)
+                record.return_date = record.launch_date + duration
     
-    fuel_amount = fields.Float(string='Amount of Fuel')
-    engine_count = fields.Integer(string='Number of Engines')
-    Safety_features = fields.Text(string='Safety Features')
-    
-    
+    def _inverse_return_date(self):
+        for record in self:
+            if record.launch_date and record.return_date:
+                record.duration = (record.return_date - record.launch_date).days
+            else:
+                continue
